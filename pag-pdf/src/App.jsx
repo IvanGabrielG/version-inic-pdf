1832ec2583ad432df492aa3eebf3661ca04244c6
Ivan Galarza <ivan.g.galar@gmail.com>
2025-09-17 18:22:48 -0300

    integración completa y corregida de página. Puesta a punto y funcionando

--- pag-pdf/src/App.jsx
+++ pag-pdf/src/App.jsx
@@ -1,4 +1,4 @@
-/*import { Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Menu from "./pages/Menu";
 import Cargar from "./pages/Cargar";
@@ -13,19 +13,6 @@ export default function App() {
         <Route path="visualizar" element={<Visualizar />} />
       </Route>
     </Routes>
-  );
-}*/
-export default function App() {
-  return (
-    <div className="min-h-screen flex items-center justify-center">
-      <div className="p-8 rounded shadow-lg bg-white">
-        <h1 className="text-3xl font-bold mb-4">TAILWIND TEST</h1>
-        <p className="mb-4">Si ves este texto con estilos Tailwind, Tailwind funciona.</p>
-        <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded">
-          Botón rojo (debería ser rojo)
-        </button>
-      </div>
-    </div>
   );
 }
 
